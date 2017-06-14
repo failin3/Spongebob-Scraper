0266734f4af610394287106a6ab4a50a8de2638b
failin3 <felix.hoens@gmail.com>
2017-06-14 18:33:40 +0200

    Created mini installer

--- run.bat
+++ run.bat
@@ -1,4 +1,19 @@
 @echo off
+
+
+
+if exist version.txt (
+set /p version= <version.txt
+) else (
 set /p version=Python 2 or 3:
+)
+echo %version% > version.txt
+
+if not exist autorun.bat (
+(echo @echo off & echo python spongebobScraper%version%.py) > autorun.bat)
+
 cd "D:\Spongebob Videos\Python Script
+
+echo %version%
+
 python spongebobScraper%version%.py
